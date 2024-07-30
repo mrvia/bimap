0bd3cf85c527a9125b9f852f5d3e3456eacfff88
Vineeth <165706978+mrvia@users.noreply.github.com>
2024-07-30 07:56:12 +0100

    feat: bimap (#1)
    * chore: git work flows
    
    * feat: go mod initialized
    
    * feat: bimap
    
    * chore: pull request template added

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # bimap
+
 Bi-directional map for Go.
+
+## Installation
+
+Use go get to install the package:
+
+```bash
+go get github.com/mrvia/bimap
+```
+
+## Usage
+
+```go
+import "github.com/mrvia/bimap"
+
+// Create a new bi-directional map
+bm := bimap.New(map[string]int{
+    "key1": 1,
+    "key2": 2,
+})
+
+// Add key-value pairs
+bm.Put("key3", 3)
+
+// Get value by key
+value, ok := bm.GetByKey("key1") // value == 1, ok == true
+
+// Get key by value
+key, ok := bm.GetByValue(2) // key == "key2", ok == true
+```
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ bimap_test.go
@@ -0,0 +1,76 @@
+package bimap
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestNewBiMap(t *testing.T) {
+	// Empty BiMap
+	emptyBM := NewBiMap[any, any](nil)
+
+	if emptyBM.Len() != 0 {
+		t.Errorf("Expected empty BiMap, got length %d", emptyBM.Len())
+	}
+
+	m := map[string]int{
+		"one": 1,
+		"two": 2,
+	}
+
+	// BiMap with initial values
+	nonEmptyBM := NewBiMap(m)
+	// Keys and Values
+	keys := nonEmptyBM.Keys()
+	values := nonEmptyBM.Values()
+
+	// Check length
+	if nonEmptyBM.Len() != len(m) {
+		t.Errorf("Expected BiMap length %d, got %d", len(m), nonEmptyBM.Len())
+	}
+
+	// Check entries
+	for k, v := range m {
+
+		if slices.Contains(keys, k) != true {
+			t.Errorf("Expected key %s in BiMap", k)
+		}
+
+		if slices.Contains(values, v) != true {
+			t.Errorf("Expected value %v in BiMap", v)
+		}
+
+		if val, ok := nonEmptyBM.GetByKey(k); !ok || val != v {
+			t.Errorf("Expected value %v for key %s, got %v", v, k, val)
+		}
+
+		if val, ok := nonEmptyBM.GetByValue(v); !ok || val != k {
+			t.Errorf("Expected key %s for value %v, got %v", k, v, val)
+		}
+	}
+
+	// Add new entry
+	nonEmptyBM.Put("three", 3)
+
+	// Check length
+	if nonEmptyBM.Len() != len(m) {
+		t.Errorf("Expected BiMap length %d, got %d", len(m)+1, nonEmptyBM.Len())
+	}
+
+	// Delete entry
+	nonEmptyBM.Delete("three")
+
+	// Check length
+	if nonEmptyBM.Len() != len(m) {
+		t.Errorf("Expected BiMap length %d, got %d", len(m), nonEmptyBM.Len())
+	}
+
+	// Clear BiMap
+	nonEmptyBM.Clear()
+
+	// Check length
+	if nonEmptyBM.Len() != 0 {
+		t.Errorf("Expected empty BiMap, got length %d", nonEmptyBM.Len())
+	}
+
+}
