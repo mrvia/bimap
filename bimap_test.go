52b31ac8c4b30c8d289bddeaf8680a28ca3cae67
Vineeth <dreys.mind@gmail.com>
2024-07-31 07:59:11 +0100

    chore: test cases updated

--- bimap_test.go
+++ bimap_test.go
@@ -6,109 +6,127 @@ import (
 )
 
 func TestNewBiMap(t *testing.T) {
-	// Empty BiMap
-	emptyBM := NewBiMap[any, any](nil)
+	t.Run("empty map", func(t *testing.T) {
+		// Empty BiMap
+		emptyBM := NewBiMap[any, any](nil)
 
-	if emptyBM.Len() != 0 {
-		t.Errorf("Expected empty BiMap, got length %d", emptyBM.Len())
-	}
+		if emptyBM.Len() != 0 {
+			t.Errorf("Expected empty BiMap, got length %d", emptyBM.Len())
+		}
+	})
 
 	m := map[string]int{
 		"one": 1,
 		"two": 2,
 	}
 
-	// BiMap with initial values
-	nonEmptyBM := NewBiMap(m)
-	// Keys and Values
-	keys := nonEmptyBM.Keys()
-	values := nonEmptyBM.Values()
+	t.Run("non-empty map", func(t *testing.T) {
 
-	// Check length
-	if nonEmptyBM.Len() != len(m) {
-		t.Errorf("Expected BiMap length %d, got %d", len(m), nonEmptyBM.Len())
-	}
+		// BiMap with initial values
+		nonEmptyBM := NewBiMap(m)
+		// Keys and Values
+		keys := nonEmptyBM.Keys()
+		values := nonEmptyBM.Values()
 
-	// Check entries
-	for k, v := range m {
-
-		if slices.Contains(keys, k) != true {
-			t.Errorf("Expected key %s in BiMap", k)
+		// Check length
+		if nonEmptyBM.Len() != len(m) {
+			t.Errorf("Expected BiMap length %d, got %d", len(m), nonEmptyBM.Len())
 		}
 
-		if slices.Contains(values, v) != true {
-			t.Errorf("Expected value %v in BiMap", v)
+		// Check entries
+		for k, v := range m {
+
+			if slices.Contains(keys, k) != true {
+				t.Errorf("Expected key %s in BiMap", k)
+			}
+
+			if slices.Contains(values, v) != true {
+				t.Errorf("Expected value %v in BiMap", v)
+			}
+
+			if val, ok := nonEmptyBM.GetByKey(k); !ok || val != v {
+				t.Errorf("Expected value %v for key %s, got %v", v, k, val)
+			}
+
+			if val, ok := nonEmptyBM.GetByValue(v); !ok || val != k {
+				t.Errorf("Expected key %s for value %v, got %v", k, v, val)
+			}
 		}
 
-		if val, ok := nonEmptyBM.GetByKey(k); !ok || val != v {
-			t.Errorf("Expected value %v for key %s, got %v", v, k, val)
+		// Add new entry
+		nonEmptyBM.Put("three", 3)
+
+		// Check length
+		if nonEmptyBM.Len() != len(m) {
+			t.Errorf("Expected BiMap length %d, got %d", len(m)+1, nonEmptyBM.Len())
 		}
 
-		if val, ok := nonEmptyBM.GetByValue(v); !ok || val != k {
-			t.Errorf("Expected key %s for value %v, got %v", k, v, val)
+		// Delete entry
+		nonEmptyBM.Delete("three")
+
+		// Check length
+		if nonEmptyBM.Len() != len(m) {
+			t.Errorf("Expected BiMap length %d, got %d", len(m), nonEmptyBM.Len())
 		}
-	}
 
-	// Add new entry
-	nonEmptyBM.Put("three", 3)
+		// Clear BiMap
+		nonEmptyBM.Clear()
 
-	// Check length
-	if nonEmptyBM.Len() != len(m) {
-		t.Errorf("Expected BiMap length %d, got %d", len(m)+1, nonEmptyBM.Len())
-	}
+		// Check length
+		if nonEmptyBM.Len() != 0 {
+			t.Errorf("Expected empty BiMap, got length %d", nonEmptyBM.Len())
+		}
+	})
 
-	// Delete entry
-	nonEmptyBM.Delete("three")
-
-	// Check length
-	if nonEmptyBM.Len() != len(m) {
-		t.Errorf("Expected BiMap length %d, got %d", len(m), nonEmptyBM.Len())
-	}
-
-	// Clear BiMap
-	nonEmptyBM.Clear()
-
-	// Check length
-	if nonEmptyBM.Len() != 0 {
-		t.Errorf("Expected empty BiMap, got length %d", nonEmptyBM.Len())
-	}
 }
 
 func TestNewReadOnlyBiMap(t *testing.T) {
 
+	t.Run("empty map", func(t *testing.T) {
+		// Empty BiMap
+		emptyBM := NewReadOnlyBiMap[any, any](nil)
+
+		if emptyBM.Len() != 0 {
+			t.Errorf("Expected empty BiMap, got length %d", emptyBM.Len())
+		}
+	})
+
 	m := map[string]int{
 		"one": 1,
 		"two": 2,
 	}
 
-	rBiMap := NewReadOnlyBiMap(m)
+	t.Run("non-empty map", func(t *testing.T) {
 
-	// Keys and Values
-	keys := rBiMap.Keys()
-	values := rBiMap.Values()
+		rBiMap := NewReadOnlyBiMap(m)
 
-	// Check length
-	if rBiMap.Len() != len(m) {
-		t.Errorf("Expected ReadOnlyBiMap length %d, got %d", len(m), rBiMap.Len())
-	}
+		// Keys and Values
+		keys := rBiMap.Keys()
+		values := rBiMap.Values()
 
-	// Check entries
-	for k, v := range m {
-
-		if slices.Contains(keys, k) != true {
-			t.Errorf("Expected key %s in BiMap", k)
+		// Check length
+		if rBiMap.Len() != len(m) {
+			t.Errorf("Expected ReadOnlyBiMap length %d, got %d", len(m), rBiMap.Len())
 		}
 
-		if slices.Contains(values, v) != true {
-			t.Errorf("Expected value %v in BiMap", v)
-		}
+		// Check entries
+		for k, v := range m {
 
-		if val, ok := rBiMap.GetByKey(k); !ok || val != v {
-			t.Errorf("Expected value %v for key %s, got %v", v, k, val)
-		}
+			if slices.Contains(keys, k) != true {
+				t.Errorf("Expected key %s in BiMap", k)
+			}
 
-		if val, ok := rBiMap.GetByValue(v); !ok || val != k {
-			t.Errorf("Expected key %s for value %v, got %v", k, v, val)
+			if slices.Contains(values, v) != true {
+				t.Errorf("Expected value %v in BiMap", v)
+			}
+
+			if val, ok := rBiMap.GetByKey(k); !ok || val != v {
+				t.Errorf("Expected value %v for key %s, got %v", v, k, val)
+			}
+
+			if val, ok := rBiMap.GetByValue(v); !ok || val != k {
+				t.Errorf("Expected key %s for value %v, got %v", k, v, val)
+			}
 		}
-	}
+	})
 }
